a914d916395a830b279360317854248d9e0ea210
chung <chungvtgc00989@fpt.edu.vn>
2016-08-31 11:59:24 +0700

    fix

--- DemoScrollView/ViewScroll.swift
+++ DemoScrollView/ViewScroll.swift
@@ -1,13 +1,85 @@
-//
-//  ViewScroll.swift
-//  DemoScrollView
-//
-//  Created by Chung on 8/30/16.
-//  Copyright © 2016 newayplus. All rights reserved.
-//
 
-import Cocoa
 
-class ViewScroll: UIViewController {
+import UIKit
 
+class ViewScroll: UIViewController, UIScrollViewDelegate {
+    var photo: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var slider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if (photo != nil)
+        {
+            return
+        }
+        let imgView = UIImageView(image: UIImage(named: "shop.jpg"))
+        imgView.frame = CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height)
+        imgView.contentMode = .ScaleAspectFit
+        photo = imgView
+        //set sự kiện click để zoomin zoomout
+        imgView.userInteractionEnabled = true
+        imgView.multipleTouchEnabled = true
+        
+        let tap = UITapGestureRecognizer(target: self, action: Selector("tapImg:"))
+        tap.numberOfTapsRequired = 1
+        imgView.addGestureRecognizer(tap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: Selector("doubleTab:"))
+        doubleTap.numberOfTapsRequired = 2
+        tap.requireGestureRecognizerToFail(doubleTap)
+        imgView.addGestureRecognizer(doubleTap)
+        
+        scrollView.contentSize = CGSizeMake(imgView.bounds.width, imgView.bounds.height)
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 2
+        
+        let scale = 1
+        let point = scrollView.center
+        zoomRectForScale(CGFloat(scale), center: point)
+        
+        self.scrollView.addSubview(imgView)
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return photo
+    }
+    func tapImg(gesture: UITapGestureRecognizer){
+        let position = gesture.locationInView(photo)
+        zoomRectForScale(scrollView.zoomScale * 1.5, center: position)
+        
+    }
+    func doubleTab(gesture: UITapGestureRecognizer){
+        let position = gesture.locationInView(photo)
+        zoomRectForScale(scrollView.zoomScale * -1.5, center: position)
+    }
+    
+    func zoomRectForScale(scale: CGFloat , center: CGPoint){
+        var zoomRect = CGRect()
+        let scrollViewSize = scrollView.bounds.size
+        zoomRect.size.height = scrollViewSize.height / scale
+        zoomRect.size.width = scrollViewSize.width / scale
+        
+        zoomRect.origin.x = center.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0)
+        
+        print(zoomRect)
+        scrollView.zoomToRect(zoomRect, animated: true)
+}
+    
+    @IBAction func slider(sender: AnyObject) {
+
+        slider.maximumValue = Float(scrollView.maximumZoomScale)
+        let scale = slider.value
+        let point = CGPointMake(scrollView.center.x + scrollView.frame.origin.x , scrollView.center.y + scrollView.frame.origin.y)
+        zoomRectForScale(CGFloat(scale), center: point)
+    }
+    
+    
+    
+    
 }
